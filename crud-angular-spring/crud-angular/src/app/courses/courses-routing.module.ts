1e8682b30de36325fdb80435caff47e7f002cadc
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-05-23 10:02:01 -0300

    Update CRUD-Angula-Sprint

--- crud-angular-spring/crud-angular/src/app/courses/courses-routing.module.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListCoursesComponent } from './list-courses/list-courses.component';
+
+import { CoursesComponent } from './containers/courses/courses.component';
+import { CourseFormComponent } from './containers/course-form/course-form.component';
+import { CourseResolver } from './guards/course.resolver';
 
 const routes: Routes = [
-  { path:'', component: ListCoursesComponent }
+  { path: '', component: CoursesComponent },
+  { path: 'new', component: CourseFormComponent, resolve: { course: CourseResolver } },
+  { path: 'edit/:id', component: CourseFormComponent, resolve: { course: CourseResolver } }
 ];
 
 @NgModule({
